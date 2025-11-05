7c19fbbc62fa260a88be4e9b1479c9966b337043
Jayse007 <shawonjames002@gmail.com>
2025-11-05 10:31:56 +0100

    / route implemented for both mobile and desktop version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/totalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Adrian', lastName: 'Cyrus', email:'Cyruse@gmail.com'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+        
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user= {loggedIn?.firstName || 'Guest'}
+            subtext= "Access and manage your accournt and \
+            transactions efficiently."
+          />
+          
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.05}
+          />
+        </header> 
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+         user={loggedIn}
+         transactions={[]}
+         banks={[{ currentBalance: 1203.50, id: "second" }, { currentBalance: 1000.21, id: "first" }]}/>
+    </section>
   )
 }
 
